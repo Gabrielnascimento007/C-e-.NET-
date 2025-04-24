52444b746e36df0eb32db4fa7f24412c8e47c0a9
Gabriel Nascimento Barbosa <gabrielnascimentoph2@gmail.com>
2025-04-24 12:00:58 -0300

    Exceptions
    Nessa pasta estão os tratamentos de erros

---
+++ ProductClientHub.Exceptions/ExceptionsBase/ErrorOnValidationException.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace ProductClientHub.Exceptions.ExceptionsBase;
+
+public class ErrorOnValidationException : ProductClientHubExceptions
+{
+    private readonly List<string> _errors;
+
+    public ErrorOnValidationException( List<string> errorsMessages) : base(string.Empty)
+    {
+        _errors = errorsMessages;
+    }
+
+    public override List<string> GetErrors() => _errors;
+
+    public override HttpStatusCode GetHttpStatusCode() => HttpStatusCode.BadRequest; 
+}
